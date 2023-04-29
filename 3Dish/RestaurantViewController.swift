c57d87ae419df3edbb8b67984881449bc9d79e55
Ronaldoh1 <2ronald.hernandez@gmail.com>
2023-04-29 11:16:38 -0700

    rename viewcontroller
    Signed-off-by: Ronaldoh1 <2ronald.hernandez@gmail.com>

---
+++ 3Dish/RestaurantViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  3Dish
+//
+//  Created by RONALD HERNANDEZ on 4/27/23.
+//
+
+import UIKit
+
+class RestaurantViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+    }
+
+
+}
+
